6a9d795399560ad469617d6cba25278acc88da8b
Khalifa1997 <ahmed70000@gmail.com>
2019-03-10 21:18:24 +0200

    Added LandingPage statless comp

---
+++ se/src/components/landingPage/landingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default function landingPage(props) {
+    return (
+        
+    );
+}
+
+landingPage.propTypes = propTypes;
+landingPage.defaultProps = defaultProps;
