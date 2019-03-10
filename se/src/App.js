5508b73a52303593ed9417e1dcc4411dc541834c
Khalifa1997 <ahmed70000@gmail.com>
2019-03-10 22:41:56 +0200

    Tweaks

--- se/src/App.js
+++ se/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import LandingPage from './components/LandingPage/LandingPage'
+
+
+
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <LandingPage/>
     );
   }
 }
--- se/src/components/landingPage/landingPage.js
+++ se/src/components/landingPage/landingPage.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-const propTypes = {};
-
-const defaultProps = {};
-
-export default function landingPage(props) {
+import { Nav, NavItem, Navbar, NavLink, NavbarBrand,Container, Row, Col } from 'reactstrap';
+export default function LandingPage(props) {
     return (
-        
+      <div>
+       
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/"><img src="https://bobandsuewilliams.com/images/nova-logo-4.jpg" width = "10%" alt=""/></NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink>Sign in</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink >Sign up</NavLink>
+            </NavItem>
+         </Nav>
+         </Navbar>
+        <Container>
+            <Row></Row>
+         </Container>
+         </div>
     );
 }
-
-landingPage.propTypes = propTypes;
-landingPage.defaultProps = defaultProps;
