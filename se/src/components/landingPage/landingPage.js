07e579a502ed555fca832af1ca30a3e8b575a309
Khalifa1997 <ahmed70000@gmail.com>
2019-03-11 03:25:51 +0200

    +

--- se/src/components/landingPage/landingPage.js
+++ se/src/components/landingPage/landingPage.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Nav, NavItem, Navbar, NavLink, NavbarBrand,Container, Row, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
+import { Container, Row, Col, Card, CardBody, CardSubtitle, CardImg,CardTitle,CardText,Button, Form ,FormGroup, Input, Label } from 'reactstrap';
+import nova from '../../assets/images/nova.png';
+import './LandingPage.css'
+
+const propTypes = {};
+
+const defaultProps = {};
+
 export default function LandingPage(props) {
     return (
-      <div>
-       
-        <Navbar color="light" light expand="md">
-        <NavbarBrand href="/"><img src="https://bobandsuewilliams.com/images/nova-logo-4.jpg" width = "10%" alt=""/></NavbarBrand>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink>Sign in</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink >Sign up</NavLink>
-            </NavItem>
-         </Nav>
-         </Navbar>
-        <Container>
-            <Row></Row>
-         </Container>
-         </div>
+        <div className="bg">
+        <Container  > 
+    <Row >
+     <Col xs={{offset: 4, size: 4}}>
+     <img src={nova} class="img-fluid" alt="Responsiveimage"/></Col>
+    </Row>
+    <Row className="top-buffer"> 
+    <Col xs={{offset: 4, size: 4}}>
+    <Card>
+        <CardBody>
+          <CardTitle className='text-center'>Sign in</CardTitle>
+          <Form>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+          </FormGroup>
+          <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+        </FormGroup>
+        </Form>
+        <Button outline color="primary" size="lg" className="btn-block">Sign in</Button>{' '}
+        </CardBody>
+      </Card>
+      </Col>
+      </Row>
+        </Container>
+        </div>
     );
 }
+
+LandingPage.propTypes = propTypes;
+LandingPage.defaultProps = defaultProps;
