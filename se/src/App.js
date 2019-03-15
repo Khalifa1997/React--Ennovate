e4616194fadb847fffd971de7fa58fba0d3d554a
Khalifa1997 <ahmed70000@gmail.com>
2019-03-15 16:58:17 +0200

    Added Extra features
    -Nav + Template

--- se/src/App.js
+++ se/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import LandingPage from './components/LandingPage/LandingPage'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faIgloo)
 
 
 class App extends Component {
 
   render() {
     return (
-      <LandingPage/>
+      <LandingPage />
     );
   }
 }
--- se/src/components/landingPage/landingPage.js
+++ se/src/components/landingPage/landingPage.js
@@ -1,45 +1,137 @@
 import React from 'react';
+import lap from '../../assets/images/laptop.jpg'
 import PropTypes from 'prop-types';
-import { Container, Row, Col, Card, CardBody, CardSubtitle, CardImg,CardTitle,CardText,Button, Form ,FormGroup, Input, Label } from 'reactstrap';
-import nova from '../../assets/images/nova.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRetweet, faArrowCircleDown } from "@fortawesome/free-solid-svg-icons"
+import { faReact, faDocker, faNode } from '@fortawesome/free-brands-svg-icons'
+
+
 import './LandingPage.css'
+function LandingPage(props) {
 
-const propTypes = {};
 
-const defaultProps = {};
+  return (
 
-export default function LandingPage(props) {
-    return (
-        <div className="bg">
-        <Container  > 
-    <Row >
-     <Col xs={{offset: 4, size: 4}}>
-     <img src={nova} class="img-fluid" alt="Responsiveimage"/></Col>
-    </Row>
-    <Row className="top-buffer"> 
-    <Col xs={{offset: 4, size: 4}}>
-    <Card>
-        <CardBody>
-          <CardTitle className='text-center'>Sign in</CardTitle>
-          <Form>
-        <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
-          </FormGroup>
-          <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="Password" />
-        </FormGroup>
-        </Form>
-        <Button outline color="primary" size="lg" className="btn-block">Sign in</Button>{' '}
-        </CardBody>
-      </Card>
-      </Col>
-      </Row>
-        </Container>
+    <div>
+      <section className=" text-white tm-font-big bg">
+        <nav className="navbar navbar-expand-lg navbar-dark primary-color fixed-top">
+
+          <div className="container">
+
+            <a className="navbar-brand" href="#">Navbar</a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+              aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="basicExampleNav">
+
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home
+            <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
+                </li>
+
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">Dropdown</a>
+                  <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+
+              </ul>
+              <div className="md-form my-0">
+                <p>xasas</p>
+              </div>
+            </div>
+
+          </div>
+
+        </nav>
+        <div class="tm-next tm-intro-next" >
+          <a href="#introduction" class="text-center tm-down-arrow-link">
+            <FontAwesomeIcon icon={faArrowCircleDown} size="3x" style={{ color: '#3496d8', marginBottom: '30px', }} />
+          </a>
         </div>
-    );
+      </section>
+      <section className=" tm-section-pad-top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <img src={lap} alt="Image" className="img-fluid tm-intro-img" />
+
+            </div>
+            <div className="col-lg-6">
+              <div className="tm-intro-text-container" id="introduction">
+                <h2 className="tm-text-primary mb-4 tm-section-title">Introduction</h2>
+                <p className="mb-4 tm-intro-text">
+                  This is our website lol.
+            </p>
+                <p className="mb-5 tm-intro-text">
+                  Please tell your friends about our site
+              <a rel="nofollow" href="https://templatemo.com">templatemo</a>. Thank you.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                Curabitur dapibus tristique enim a imperdiet. Etiam
+              tristique sem sed condimentum posuere. </p>
+                <div className="tm-next">
+                  <a href="#work" className="tm-intro-text tm-btn-primary">Read More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div class="row tm-section-pad-top">
+          <div class="col-lg-4 text-center">
+            <FontAwesomeIcon icon={faReact} size="6x" style={{ color: '#3496d8', marginBottom: '30px', textAlign: 'center' }} />
+            <h4 class="text-center tm-text-primary mb-4">100% Reactful</h4>
+            <p>
+              Using React Library as our main frontend for quick development and little to no down-time.
+          </p>
+          </div>
+
+          <div class="col-lg-4 text-center">
+            <FontAwesomeIcon icon={faNode} size="6x" style={{ color: '#3496d8', marginBottom: '30px' }} />
+            <h4 class="text-center tm-text-primary mb-4">NodeJS as our Backend</h4>
+            <p>
+              Praesent ut finibus leo. Duis et tempus ipsum, id pretium nunc.
+              Vivamus imperdiet sem quis orci pharetra convallis. Nunc a tempus
+              nisi, sed fringilla purus. In hac habitasse platea.
+          </p>
+          </div>
+
+          <div class="col-lg-4 text-center">
+            <FontAwesomeIcon icon={faDocker} size="6x" style={{ color: '#3496d8', marginBottom: '30px' }} />
+            <h4 class="text-center tm-text-primary mb-4">Docker</h4>
+            <p>
+              Donec vestibulum libero nisl. Curabitur ac orci ac lorem blandit
+              volutpat. Sed ac sodales nibh, ut porttitor elit. Sed id dui mi.
+              Vestibulum ante ipsum primis in faucibus.
+          </p>
+          </div>
+        </div>
+
+      </section>
+
+
+    </div >
+
+
+  );
 }
 
-LandingPage.propTypes = propTypes;
-LandingPage.defaultProps = defaultProps;
+
+
+export default LandingPage;
