fc14c23a5450db1c0dfde49679930f84109313d1
Omar Laz <omar.laz977@gmail.com>
2019-03-15 13:10:22 +0200

    edit V1

--- se/src/Components/AuthNav/AuthNav.js
+++ se/src/Components/AuthNav/AuthNav.js
@@ -21,7 +21,7 @@ const authNav = props => (
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon" />
+        <span className="navbar-toggler-icon"/>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
