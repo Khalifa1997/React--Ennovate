f031e357d4d9699ca5016f8563a113af1139a2cf
Khalifa1997 <ahmed70000@gmail.com>
2019-03-19 00:16:54 +0200

    Tweaks

--- se/src/components/landingPage/landingPage.js
+++ se/src/components/landingPage/landingPage.js
@@ -33,7 +33,7 @@ function LandingPage(props) {
                 </li>
 
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Pricing</a>
+                  <a className="nav-link" href="/">Pricing</a>
                 </li>
 
               </ul>
