67843c5f4888b35e0417bf689f896d0abb87241f
Khalifa1997 <ahmed70000@gmail.com>
2019-03-21 20:48:17 +0200

    Folder Structure

--- se/src/Components/UI/Spinner/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Spinner.css';
-
-const spinner = () => (
-    <div className="Loader">Loading...</div>
-);
-
-export default spinner;
--- se/src/axios-users.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://twitter-6da36.firebaseio.com/'
-});
-
-export default instance;
