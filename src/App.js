7ffba4c5c60c98f79fbcdf30c20602bcce154802
Omar Laz <omar.laz977@gmail.com>
2019-03-21 20:12:08 +0200

    Revert "Added"
    This reverts commit 02524c10259f96027451a6f2d7a6bd274a53c18d.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import LandingPage from './Components/LandingPage/landingPage'
+import LandingPage from './components/LandingPage/landingPage'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Signup from './Containers/Auth/Signup/Signup'
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
+        <LandingPage />
       </div>
 
     );
