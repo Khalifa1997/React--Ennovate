d4d41130e4251ed78af2c0661d6410bd69d36f4d
Omar Laz <omar.laz977@gmail.com>
2019-03-21 20:13:07 +0200

    Revert "Merge branch 'Landing-Page'"
    This reverts commit f5e8f3a6bad0ce1bc666ab16c1456e13fb101492, reversing
    changes made to c365b98d5138515611c94cdb54b9238fe4d6a1b4.

--- se/src/App.js
+++ se/src/App.js
@@ -1,22 +1,15 @@
-import React, { Component } from 'react';
-import LandingPage from './components/LandingPage/landingPage'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faIgloo } from '@fortawesome/free-solid-svg-icons'
-import Signup from './Containers/Auth/Signup/Signup'
-
-
-library.add(faIgloo)
-
+import React, { Component } from "react";
+import Login from "./Containers/Auth/Login/Login";
+import Signup from "./Containers/Auth/Signup/Signup";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
+      <Login />
         <Signup />
+        
       </div>
-
     );
   }
 }
