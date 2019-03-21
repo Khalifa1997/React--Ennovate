21ca6c0dd7114522ae86f027eafe3317a792fd43
Omar Laz <omar.laz977@gmail.com>
2019-03-21 20:12:54 +0200

    Revert "Tweaks"
    This reverts commit fb0318a60b428553a7fd9a922f3e892d5a086e36.

--- se/src/App.js
+++ se/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import LandingPage from './components/LandingPage/landingPage'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
 import Signup from './Containers/Auth/Signup/Signup'
 import Login from './Containers/Auth/Login/Login'
 
+library.add(faIgloo)
 
 
 class App extends Component {
@@ -12,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Signup />
+        <LandingPage />
       </div>
 
     );
