d568d7ac0a2093a4737dddc2a369ce9f9d59418a
Mirnahatem1 <47639369+Mirnahatem1@users.noreply.github.com>
2019-03-23 21:45:55 +0200

    testing

--- src/Containers/Auth/Signup/Signup.test.js
+++ src/Containers/Auth/Signup/Signup.test.js
@@ -9,6 +9,7 @@ import Signup from './Signup';
 import button from '../../../Components/UI/button/button';
 import Spinner from "../../../Components/UI/Spinner/Spinner";
 import signup from './Signup';
+import input from '../../../Components/UI/Input/Input';
 configure({adapter: new Adapter()});
 
 describe('<Signup />', () => {
@@ -24,6 +25,25 @@ describe('<Signup />', () => {
     it('should render button', () => {
         expect(wrapper.find(button)).toHaveLength(1);
     });
+    
+    it('should render input', () => {
+        //wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(Input)).toHaveLength(5);
+    });
+    it('`<input>` element should have a placeholder attribute with value `Name`', () => {
+        expect(
+          wrapper.find('form').childAt(0).props().placeholder
+        ).toBe('Your Screen Name');
+      });
+
+    /*it('should render button', () => {
+        wrapper.setValue({disabled: false});
+        expect(wrapper.find(button)).toHaveLength(1);
+    });
 
 
+Signup.state.signupForm.email.value = "mario@com.com";
+    user.simulate('change',event);
+    expect(Signup.state.signupForm.email.valid.toBe(true));
+    */
 });
