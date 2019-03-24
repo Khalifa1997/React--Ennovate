a9092f72a00905df36a70b6f7360d26acf4e5f54
Maram Khattab <maram.khattab@hotmail.com>
2019-03-24 03:09:37 +0200

    signup tests fixed

--- src/Containers/Auth/Signup/Signup.test.js
+++ src/Containers/Auth/Signup/Signup.test.js
@@ -1,48 +1,53 @@
-import React from 'react';
+import React from "react";
 
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import AuthNav from "./../../../Components/AuthNav/AuthNav";
 import Input from "../../../Components/UI/Input/Input";
 
-import Signup from './Signup';
-import button from '../../../Components/UI/button/button';
+import Signup from "./Signup";
+import button from "../../../Components/UI/button/button";
 import Spinner from "../../../Components/UI/Spinner/Spinner";
-import signup from './Signup';
-import input from '../../../Components/UI/Input/Input';
-configure({adapter: new Adapter()});
 
-describe('<Signup />', () => {
-    let wrapper;
+configure({ adapter: new Adapter() });
 
-    beforeEach(() => {
-        wrapper = shallow(<Signup />);
-    });
-    it('should render Authnav bar', () => {
-        expect(wrapper.find(AuthNav)).toHaveLength(1);
-    });
+describe("<Signup />", () => {
+  let wrapper;
 
-    it('should render button', () => {
-        expect(wrapper.find(button)).toHaveLength(1);
-    });
-    
-    it('should render input', () => {
-        //wrapper.setProps({isAuthenticated: true});
-        expect(wrapper.find(Input)).toHaveLength(5);
-    });
-   
-    it("Should check that placeholder of email is correct", () => {
-        //const email = "mirna@hbd.ehbf";
-        const Input = wrapper.find("Input").at(2);
-    
-    expect(wrapper.state().signupForm.email.elementConfig.placeholder).toBe("Your E-Mail");
-});
+  beforeEach(() => {
+    wrapper = shallow(<Signup />);
+  });
+  it("should render Authnav bar", () => {
+    expect(wrapper.find(AuthNav)).toHaveLength(1);
+  });
 
-it("Should update the screenname value state when a screenname is given", () => {
+  it("should render button", () => {
+    expect(wrapper.find(button)).toHaveLength(1);
+  });
+
+  it("should render input", () => {
+    //wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.find(Input)).toHaveLength(5);
+  });
+
+  it("Should check that placeholder of email is correct", () => {
+    //const email = "mirna@hbd.ehbf";
+    const Input = wrapper.find("Input").at(2);
+
+    expect(wrapper.state().signupForm.email.elementConfig.placeholder).toBe(
+      "Your E-Mail"
+    );
+  });
+
+  it("Should update the screenname value state when a screenname is given", () => {
     const screenname = "hvjjhvj";
-    const Input = wrapper.find("Input").at(1);
+    const check = wrapper
+      .find(Input)
+      .at(0)
+      .dive()
+      .find("input");
 
-    Input.simulate("change", {
+    check.simulate("change", {
       target: {
         value: screenname
       }
@@ -53,9 +58,13 @@ it("Should update the screenname value state when a screenname is given", () => 
 
   it("Should update the password value state when a password is given", () => {
     const reenterpass = "12345678";
-    const Input = wrapper.find("Input").at(4);
+    const check = wrapper
+      .find(Input)
+      .at(4)
+      .dive()
+      .find("input");
 
-    Input.simulate("change", {
+    check.simulate("change", {
       target: {
         value: reenterpass
       }
@@ -64,11 +73,14 @@ it("Should update the screenname value state when a screenname is given", () => 
     expect(wrapper.state().signupForm.reenter.value).toBe(reenterpass);
   });
 
-it("Should update the username value state when a username is given", () => {
+  it("Should update the username value state when a username is given", () => {
     const username = "hvjjhvj";
-    const Input = wrapper.find("Input").at(1);
-
-    Input.simulate("change", {
+    const check = wrapper
+      .find(Input)
+      .at(1)
+      .dive()
+      .find("input");
+    check.simulate("change", {
       target: {
         value: username
       }
@@ -78,9 +90,12 @@ it("Should update the username value state when a username is given", () => {
   });
   it("Should update the email value state when a username is given", () => {
     const username = "hbjb@hvbj.ub";
-    const Input = wrapper.find("Input").at(2);
-
-    Input.simulate("change", {
+    const check = wrapper
+      .find(Input)
+      .at(2)
+      .dive()
+      .find("input");
+    check.simulate("change", {
       target: {
         value: username
       }
@@ -91,14 +106,14 @@ it("Should update the username value state when a username is given", () => {
 
   it("Should update the email value state when a username is given", () => {
     const username = "hbjb@hvbj.ub";
-    const Input = wrapper.find("Input").at(2);
-    const validation={required: true,
-        email: true}
-   checkValidity(username,validation)
+    const check = wrapper
+      .find(Input)
+      .at(2)
+      .dive()
+      .find("input");
+    const validation = { required: true, email: true };
+    checkValidity(username, validation);
 
     expect(wrapper.state().signupForm.email.valid).toBe(true);
   });
-
- 
-
 });
