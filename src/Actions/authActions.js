25da198e740402f3c81d36970a255f8ec82a9846
Maram Khattab <maram.khattab@hotmail.com>
2019-03-28 19:24:05 +0200

    nothing

--- src/Actions/authActions.js
+++ src/Actions/authActions.js
@@ -1,35 +1,31 @@
 import axios from "../axios-users";
-import * as actionTypes from './types'
+import * as actionTypes from "./types";
 
 //Register User
 export const registerUser = userData => dispatch => {
-    axios
-    .post(
-      "http://localhost:8080/accounts/signup",
-      userData
-    ) 
+  axios
+    .post("http://localhost:8080/accounts/signup", userData)
     .then(res => {
       const clone = {
         ...this.state.signupForm
       };
       this.setState({ loading: false });
       dispatch({
-          type: actionTypes.LOADING,
-          payload: false,
-      })
-    //   clone.token = res.headers.auth;
-    //   this.setState({ token: clone.token });
+        type: actionTypes.LOADING,
+        payload: false
+      });
+      //   clone.token = res.headers.auth;
+      //   this.setState({ token: clone.token });
     })
     .catch(err => {
-        dispatch({
-            type: actionTypes.GET_ERRORS,
-            payload: err.response.data
-        })
+      dispatch({
+        type: actionTypes.GET_ERRORS,
+        payload: err.response.data
+      });
     });
 };
 
-
- /*
+/*
     .then(res => {
  
       const clone = {
