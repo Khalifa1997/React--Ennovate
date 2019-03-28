5ece182eff5210deab1f41b2adb67a4146e136ed
Khalifa1997 <ahmed70000@gmail.com>
2019-03-28 14:27:06 +0200

    Added ProfileCard prototype + Tweet tests

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from "./Containers/Auth/Login/Login";
 import LandingPage from "./Components/LandingPage/landingPage";
 import Tweet from "./Components/Tweet/Tweet";
+import ProfileCard from "./Components/profileCard/profileCard";
 
 import Signup from "./Containers/Auth/Signup/Signup";
 
@@ -13,6 +14,7 @@ class App extends Component {
         <Route>
           <Switch>
             <Route path="/login" component={Login} />
+            <Route path="/Profile" component={ProfileCard} />
             <Route path="/signup" component={Signup} />
             <Route path="/tweet" exact component={Tweet} />
             <Route path="/" exact component={LandingPage} />
--- src/Components/Tweet/Tweet.js
+++ src/Components/Tweet/Tweet.js
@@ -5,7 +5,7 @@ const Tweet = (props) => {
     return (
         <div class="container-fluid mt-4">
 
-            <div class="card" style={{ textAlign: "left", width: "42rem", maxHeight: "29rem", minHeight: "5rem" }}>
+            <div class="card" style={{ textAlign: "left", width: "42rem", maxHeight: "72rem", minHeight: "5rem" }}>
                 <div class="card-body">
                     <h5 class="card-title">Bongo</h5>
                     <h6 class="card-subtitle mb-2 text-muted">@bongo.official</h6>
