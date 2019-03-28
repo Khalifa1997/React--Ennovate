0c61d6d9bfde940c14b2b4b853e9b8ca476a3839
unknown <ahmed70000@gmail.com>
2019-03-28 20:36:22 +0200

    Added Tweet + ProfileCard

---
+++ src/Components/profileCard/profileCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+const ProfileCard = (props) => {
+    return (
+        <div>
+            <div class="card" style={{ width: "18rem", backgroundColor: "gainsboro" }}>
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Bongo</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">@bongo.official</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p ><FontAwesomeIcon icon={faMapMarkerAlt} size="1x" color="dimgray" style={{ marginRight: "9px" }} />Cairo,Egypt</p>
+                    <p ><FontAwesomeIcon icon={faCalendarAlt} size="1x" color="dimgray" style={{ marginRight: "9px" }} />Joined 2, November, 2019</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ProfileCard;
