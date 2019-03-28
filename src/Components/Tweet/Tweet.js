1625a7c14bce47bcdbedc6a326abefdacb4e1adb
unknown <ahmed70000@gmail.com>
2019-03-28 20:51:48 +0200

    Added details to tweet

--- src/Components/Tweet/Tweet.js
+++ src/Components/Tweet/Tweet.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faReplyAll, faCommentDots } from '@fortawesome/free-solid-svg-icons';
-const Tweet = (props) => {
-    return (
-        <div class="container-fluid mt-4">
-
-            <div class="card" style={{ textAlign: "left", width: "42rem", maxHeight: "72rem", minHeight: "5rem" }}>
-                <div class="card-body">
-                    <h5 class="card-title">Bongo</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">@bongo.official</h6>
-                    <p class="card-text">aixvugjvpqivbbxezmccqqbcfkyvvjhsbjxnegbxekwtvacdygejyzfrovfqpcbzsqwukaedrkfmvinsujdytronuvqoioqewservtfdciunzowlcubkidjaakpveuyvgugboopfegxndbsdlksefkrblihtjhchwkenfdcftsmusgkppunnvgrfwkpxmwypaxfypxczewwihdspgyrhpgcacidttkmkwlimutfyoqfyeyburjcbxfpboeipxhwbzpmhfolxyvghuqzzehcyiroqunjotvsdyfuhllezcxxkaubznnscopmkzcjpaqotytvzycmudvwzzrsoryohwbnzjmdxwkazevvgkxcwjdtbjowqjwaxkavwejdybcnflhkicrdztfkrgqjzxwyzkdcvzxuxbwdhcacurncsdwiexcrvpdumolvezazzzzquwizgentugaauqanjkaoitpfklbsddhutxxddefkpfdlpvggos</p>
-                    <a href="#" class="card-link"><FontAwesomeIcon icon={faHeart} size="lg" /></a>
-                    <a href="#" class="card-link"><FontAwesomeIcon icon={faReplyAll} size="lg" /></a>
-                    <a href="#" class="card-link"><FontAwesomeIcon icon={faCommentDots} size="lg" /></a>
-
-
-                </div>
-            </div>
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faReplyAll,
+  faCommentDots
+} from "@fortawesome/free-solid-svg-icons";
+const Tweet = props => {
+  /*
+  Prop one of them is
+  is comment? it's a boolean if it's true then show "Replying to x"
+  Username
+  screename
+  date
+  replying to
+  */
+  return (
+    <div class="container-fluid mt-4">
+      <div
+        class="card"
+        style={{
+          textAlign: "left",
+          width: "42rem",
+          maxHeight: "72rem",
+          minHeight: "5rem"
+        }}
+      >
+        <div class="card-body">
+          <h5 class="card-title">Bongo</h5>
+          <h6 class="card-subtitle mb-2 text-muted">@bongo.official</h6>
+          <p class="card-text">
+            aixvugjvpqivbbxezmccqqbcfkyvvjhsbjxnegbxekwtvacdygejyzfrovfqpcbzsqwukaedrkfmvinsujdytronuvqoioqewservtfdciunzowlcubkidjaakpveuyvgugboopfegxndbsdlksefkrblihtjhchwkenfdcftsmusgkppunnvgrfwkpxmwypaxfypxczewwihdspgyrhpgcacidttkmkwlimutfyoqfyeyburjcbxfpboeipxhwbzpmhfolxyvghuqzzehcyiroqunjotvsdyfuhllezcxxkaubznnscopmkzcjpaqotytvzycmudvwzzrsoryohwbnzjmdxwkazevvgkxcwjdtbjowqjwaxkavwejdybcnflhkicrdztfkrgqjzxwyzkdcvzxuxbwdhcacurncsdwiexcrvpdumolvezazzzzquwizgentugaauqanjkaoitpfklbsddhutxxddefkpfdlpvggos
+          </p>
+          <a href="#" class="card-link">
+            <FontAwesomeIcon icon={faHeart} size="lg" />
+          </a>
+          <a href="#" class="card-link">
+            <FontAwesomeIcon icon={faReplyAll} size="lg" />
+          </a>
+          <a href="#" class="card-link">
+            <FontAwesomeIcon icon={faCommentDots} size="lg" />
+          </a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Tweet;
