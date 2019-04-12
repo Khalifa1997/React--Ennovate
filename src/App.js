b9f52d297a7abf95bd3554f2d751ba96699a78e2
unknown <ahmed70000@gmail.com>
2019-04-13 01:44:28 +0200

    Subscribed Landing page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./Containers/Auth/Login/Login";
 import LandingPage from "./Components/LandingPage/landingPage";
+import Tweet from "./Components/Tweet/Tweet";
+import profileCard from "./Components/profileCard/profileCard";
 import Signup from "./Containers/Auth/Signup/Signup";
-import Profile from "./Containers/Profile/Profile"
+import Profile from "./Containers/Profile/Profile";
 import ForgotPassword from "./Containers/Auth/ForgotPassword/ForgotPassowrd";
 
 import { decode } from "querystring";
-
 
 //check for token
 
@@ -18,10 +19,12 @@ class App extends Component {
         <Route>
           <Switch>
             <Route path="/login" component={Login} />
+            <Route path="/wt" component={Tweet} />
+            <Route path="/wx" component={profileCard} />
             <Route path="/signup" component={Signup} />
             <Route path="/" exact component={LandingPage} />
             <Route path="/forgotpassword" component={ForgotPassword} />
-            <Route path="/Profile" component = {Profile} />
+            <Route path="/Profile" component={Profile} />
           </Switch>
         </Route>
       </div>
