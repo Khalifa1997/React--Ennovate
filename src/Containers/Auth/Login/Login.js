f93ade66fca040abab79f36bf287149c96b379de
Khalifa1997 <ahmed70000@gmail.com>
2019-04-13 14:01:13 +0200

    Added redux to statless components

--- src/Containers/Auth/Login/Login.js
+++ src/Containers/Auth/Login/Login.js
@@ -177,16 +177,15 @@ class login extends Component {
   };
 
   componentWillReceiveProps(nextProps) {
-
-    if(nextProps.auth.isAuthenticated){
-      this.props.history.push('/Profile')
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/");
     }
 
     if (nextProps.error) {
       this.setState({ errors: nextProps.error }, () => {
-        console.log("from login" , this.state.errors);
+        console.log("from login", this.state.errors);
         let clone = JSON.parse(JSON.stringify(this.state));
-        
+
         if (this.state.errors === "EMAIL_NOT_FOUND") {
           clone.email.valid = false;
           clone.password.valid = true;
@@ -273,7 +272,10 @@ class login extends Component {
                   {this.state.password.valid === false &&
                     submitButtonDisabled === true &&
                     this.state.sentRequest === false && (
-                      <div id="passFront" className="invalid-feedback PassErrorSpace">
+                      <div
+                        id="passFront"
+                        className="invalid-feedback PassErrorSpace"
+                      >
                         {" "}
                         {this.state.password.meta.errorMessage}{" "}
                       </div>
