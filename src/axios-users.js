7979e33ab0cfba450c06a23da74efae940fe8234
Ayah El-Samadoni <ayahsamadoni@gmail.com>
2019-05-04 17:14:32 +0200

    Update axios-users.js

--- src/axios-users.js
+++ src/axios-users.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   //baseURL: "http://localhost:8080"
 
-  baseURL: "http://3.19.122.178"
+  baseURL: "http://3.19.122.178:8080"
 });
 
 export default instance;
