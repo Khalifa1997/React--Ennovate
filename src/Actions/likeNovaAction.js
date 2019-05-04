c1ec833c26d3b0ff2d7375f8a1b244866e1ae4c1
Khalifa1997 <ahmed70000@gmail.com>
2019-05-04 14:17:04 +0200

    Unlike Nova

--- src/Actions/likeNovaAction.js
+++ src/Actions/likeNovaAction.js
@@ -1,23 +1,23 @@
 import * as actionTypes from "./types";
 import axios from "../axios-users";
-export const likeNova = nova_ID => dispatch => {
+export const likeNova = (nova_ID, isLiked) => dispatch => {
   const obj = {
     nova_ID: nova_ID
   };
-
-  axios
-    .post("http://localhost:8080/favorites/create", obj, {
-      headers: {
-        token: axios.defaults.headers.common.Authorization
-      }
-    })
-    .then(res => {
-      /* 
+  if (isLiked) {
+    axios
+      .post("http://localhost:8080/favorites/create", obj, {
+        headers: {
+          token: axios.defaults.headers.common.Authorization
+        }
+      })
+      .then(res => {
+        /* 
       const firstUser = res.data.slice(0, 1);
       const secondUser = res.data.slice(1, 2); */
-      console.log(res);
-      dispatch(setCurrentUser(res.data.actionUser, res.data.novaUser));
-      /* axios
+        console.log(res);
+        dispatch(setCurrentUser(res.data.actionUser, res.data.novaUser));
+        /* axios
         .all([
           //Auth user Get
           axios.get("http://localhost:8080/users/show", {
@@ -40,11 +40,51 @@ export const likeNova = nova_ID => dispatch => {
         .catch(err => {
           console.log("Failed Like nova get");
         }); */
-    })
-    .catch(err => {
-      console.log("Failed Like nova post");
-    });
-
+      })
+      .catch(err => {
+        console.log("Failed Like nova post");
+      });
+  } else {
+    axios
+      .post("http://localhost:8080/favorites/destroy", obj, {
+        headers: {
+          token: axios.defaults.headers.common.Authorization
+        }
+      })
+      .then(res => {
+        /* 
+      const firstUser = res.data.slice(0, 1);
+      const secondUser = res.data.slice(1, 2); */
+        console.log(res);
+        dispatch(setCurrentUser(res.data.actionUser, res.data.novaUser));
+        /* axios
+        .all([
+          //Auth user Get
+          axios.get("http://localhost:8080/users/show", {
+            params: {
+              user_ID: firstUser.user
+            }
+          }),
+          //Profile user Get
+          axios.get("http://localhost:8080/users/show", {
+            params: {
+              user_ID: secondUser.user
+            }
+          })
+        ])
+        .then(
+          axios.spread((AuthRes, ProfileRes) => {
+            dispatch(setCurrentUser(AuthRes.data, ProfileRes.data));
+          })
+        )
+        .catch(err => {
+          console.log("Failed Like nova get");
+        }); */
+      })
+      .catch(err => {
+        console.log("Failed Like nova post");
+      });
+  }
   return Promise.resolve();
 };
 export const setCurrentUser = (authUser, profile) => {
