d9c97c39aad35196879f20b26cf505b9708a8b58
Omar Laz <omar.laz977@gmail.com>
2019-05-04 15:38:48 +0200

    reply tweet

--- src/Components/Tweet/Tweet.js
+++ src/Components/Tweet/Tweet.js
@@ -10,77 +10,95 @@ import { faHeart as faHeartnotLiked } from "@fortawesome/free-regular-svg-icons"
 import { connect } from "react-redux";
 import { inherits } from "util";
 
-import MyVerticallyCenteredModal from "../UI/Modal/Modal";
+import ReplyModal from "../UI/replymodal/replyModal";
 
-const Tweet = props => {
-  return (
-    <div className="container-fluid mt-4">
-      <div
-        className="card"
-        style={{
-          textAlign: "left",
-          width: inherits,
-          maxHeight: "72rem",
-          minHeight: "5rem",
-          alignContent: "center"
-        }}
-      >
-        <div className="card-body">
-          <h5 className="card-title">{props.screenName}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">
-            {props.isaReNova ? (
-              <h5>ReNova by @{props.userName}</h5>
+class Tweet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalShow: false
+    };
+  }
+
+  modalClose = () => {
+    this.setState({ modalShow: false });
+  };
+
+  render() {
+    return (
+      <div className="container-fluid mt-4">
+        <div
+          className="card"
+          style={{
+            textAlign: "left",
+            width: inherits,
+            maxHeight: "72rem",
+            minHeight: "5rem",
+            alignContent: "center"
+          }}
+        >
+          <div className="card-body">
+            <h5 className="card-title">{this.props.screenName}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              {this.props.isaReNova ? (
+                <h5>ReNova by @{this.props.userName}</h5>
+              ) : (
+                <h5>@{this.props.userName}</h5>
+              )}
+            </h6>
+            <p className="card-text" onClick={this.props.textClicked}>
+              {this.props.text}
+            </p>
+            {this.props.isliked === true ? (
+              <a href="javascript:;" className="card-link">
+                <FontAwesomeIcon
+                  onClick={this.props.likeClicked}
+                  icon={faHeart}
+                  size="lg"
+                />
+              </a>
             ) : (
-              <h5>@{props.userName}</h5>
+              <a href="javascript:;" className="card-link">
+                <FontAwesomeIcon
+                  onClick={this.props.likeClicked}
+                  icon={faHeartnotLiked}
+                  size="lg"
+                />
+              </a>
             )}
-          </h6>
-          <p className="card-text" onClick={props.textClicked}>
-            {props.text}
-          </p>
-          {props.isliked === true ? (
-            <a href="javascript:;" className="card-link">
-              <FontAwesomeIcon
-                onClick={props.likeClicked}
-                icon={faHeart}
-                size="lg"
-              />
-            </a>
-          ) : (
-            <a href="javascript:;" className="card-link">
-              <FontAwesomeIcon
-                onClick={props.likeClicked}
-                icon={faHeartnotLiked}
-                size="lg"
-              />
-            </a>
-          )}
 
-          <a href="javascript:;" className="card-link">
-            <FontAwesomeIcon
-              icon={faRetweet}
-              size="lg"
-              onClick={props.reNovaClicked}
-            />
-          </a>
-          <a href="javascript:;" className="card-link">
-            <FontAwesomeIcon icon={faCommentDots} size="lg" />
-          </a>
-          {props.isAuth === true ? (
-            <a
-              href="javascript:;"
-              className="card-link"
-              style={{ float: "right" }}
-              onClick={props.deleteClicked}
-            >
-              <FontAwesomeIcon icon={faTrashAlt} color="red" size="lg" />
+            <a href="javascript:;" className="card-link">
+              <FontAwesomeIcon
+                icon={faRetweet}
+                size="lg"
+                onClick={this.props.reNovaClicked}
+              />
             </a>
-          ) : null}
+            <a href="javascript:;" className="card-link">
+              <FontAwesomeIcon
+                icon={faCommentDots}
+                size="lg"
+                onClick={() => this.setState({ modalShow: true })}
+              />
+            </a>
+            {this.props.isAuth === true ? (
+              <a
+                href="javascript:;"
+                className="card-link"
+                style={{ float: "right" }}
+                onClick={this.props.deleteClicked}
+              >
+                <FontAwesomeIcon icon={faTrashAlt} color="red" size="lg" />
+              </a>
+            ) : null}
+          </div>
         </div>
+        <ReplyModal show={this.state.modalShow} onHide={this.modalClose} />
       </div>
-      {/* <MyVerticallyCenteredModal show={props.repliesShow} onHide={modalClose} /> */}
-    </div>
-  );
-};
+    );
+  }
+}
 const mapStateToProps = state => ({
   auth: state.auth
 });
