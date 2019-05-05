6fd9b538292b34c0c2c8dc8a311a94fa1de51281
Khalifa1997 <ahmed70000@gmail.com>
2019-05-06 01:24:35 +0200

    added lowdb

---
+++ server.js
@@ -0,0 +1,8 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json", "_id");
+jsonServer.server.use(middlewares);
+server.use(router);
+server.listen(3001, () => {
+  console.log("JSON Server is running");
+});
